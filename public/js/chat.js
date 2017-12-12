8051d6f04c99f582946daafa7877d6f831607aa3
Ben <ben@sixgables.com>
2017-12-12 04:30:52 -0500

    add autoscroll and more documentation for front-end

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,6 @@
 var socket = io();
 
+// get required DOM elements
 var form     = document.getElementById('chatForm'); 
 var messages = document.getElementById('chatText'); // list of messages
 var input    = document.getElementById('chatInput');
@@ -12,7 +13,7 @@ socket.on('connect', () => {
     socket.emit("joinRoom", window.location.pathname.slice(6));
 });
 
-
+// when the user sends a message
 chatForm.onsubmit = function(e){
     e.preventDefault(); // stop the page from refreshing on enter
     
@@ -28,6 +29,10 @@ chatForm.onsubmit = function(e){
 */
 
 socket.on('newMsg', function(data){
+    // create the message
     formattedMessage = data.id + " | " + data.msg;
+    // add a message
     messages.innerHTML += "<div class=\"message\">" + formattedMessage + "</div>";
+
+    messages.scrollTop = messages.scrollHeight; // autoscroll
 });
